e4e50aa40c931a7ae126b2eb5bd1c1515f12d1e7
zwbrant <zwbrant@gmail.com>
2018-11-15 18:16:21 -0800

    Adds two new projects

---
+++ unet-test-client-1/Assets/Scripts/Client.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Client : MonoBehaviour {
+
+    private const int PORT = 12345;
+    private const string SERVER_IP = "127.0.0.1";
+
+    private int _hostId;
+    private byte _reliableChannel;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Connnect()
+    {
+        DontDestroyOnLoad(gameObject);
+
+        NetworkTransport.Init();
+
+        ConnectionConfig cc = new ConnectionConfig();
+        _reliableChannel = cc.AddChannel(QosType.Reliable);
+
+        HostTopology hostTopology = new HostTopology(cc, 100);
+
+        _hostId = NetworkTransport.AddHost(hostTopology, 0);
+
+        byte error;
+        NetworkTransport.Connect(_hostId, SERVER_IP, PORT, 0, out error);
+
+        if ((NetworkError)error != NetworkError.Ok)
+        {
+            //Output this message in the console with the Network Error
+            Debug.Log("There was this error : " + (NetworkError)error);
+        }
+        //Otherwise if no errors occur, output this message to the console
+        else Debug.Log("Connected : " + (NetworkError)error);
+
+
+        int i = 55555555;
+        byte b = (byte)i;
+        print(b);
+    }
+
+    public void OnConnectedToServer()
+    {
+        print("Connected to Server");
+    }
+}
---
+++ unet-test-server-1/Assets/Server.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.SceneManagement;
+
+public class Server : MonoBehaviour {
+
+    private const int PORT = 12345;
+    private const int MSG_SIZE = 1024;
+
+    private int _hostId;
+    private byte _reliableChannel;
+    private byte _error;
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+    public void Init()
+    {
+        DontDestroyOnLoad(gameObject);
+
+        NetworkTransport.Init();
+
+        ConnectionConfig cc = new ConnectionConfig();
+        _reliableChannel = cc.AddChannel(QosType.Reliable);
+
+        HostTopology hostTopology = new HostTopology(cc, 100);
+
+        _hostId = NetworkTransport.AddHost(hostTopology, PORT, null);
+
+        Debug.Log(string.Format("Opening port {0} on localhost", PORT.ToString()));
+
+    }
+
+    private void ReceiveMessage()
+    {
+        int recHostId;
+        int connectionId;
+        int channelId;
+        byte[] recBuffer = new byte[MSG_SIZE];
+        int bufferSize = 1024;
+        int dataSize;
+
+        NetworkEventType recData = NetworkTransport.Receive(out recHostId, out connectionId, out channelId, recBuffer, bufferSize, out dataSize, out _error);
+        switch (recData)
+        {
+            case NetworkEventType.Nothing: break;
+            case NetworkEventType.ConnectEvent: break;
+            case NetworkEventType.DataEvent: break;
+            case NetworkEventType.DisconnectEvent: break;
+
+            case NetworkEventType.BroadcastEvent:
+
+                break;
+        }
+    }
+}
